c2eb0a527600c510883409761ed66b0f9239d5cd
MohanKudimi <mohanchandu237@gmail.com>
2021-06-28 09:10:33 +0530

    Add project files.

---
+++ GenericMethodExample.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsDemo
+{
+    class GenericMethodExample<T>
+    {
+        private T[] _data = new T[10];
+        //passing generic parameters to a method
+        public void Method1(int index,T Name)
+        {
+            if (index >= 0 && index < 10)
+                _data[index] = Name;
+        }
+        //other generic method ways- it can be a mix of normal and generic parameters
+        public T Method2(T value1, T value2)
+        {
+            return value1;
+
+        }
+        public T Method3(T value1,  int value2)
+        {
+            return value1;
+        }
+        public T Method4(T value)
+        {
+            return value;
+        }
+    }
+    class 
+
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GenericsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiante the class using required datatype
+            CustomerData<string, int> customerdata1 = new CustomerData<string, int>();
+            customerdata1.name = "Priya";
+            customerdata1.id = 10;
+            CustomerData<string, int> customerdata2 = new CustomerData<string, int>();
+            //customerdata2.name = 20;  //gives compile time error, since string should be passed
+            //customerdata2.id = "Lakshmi"; //gives compile time error, since int should be passed
+            customerdata2.id = 20;
+            customerdata2.name = "Lakshmi";
+
+            //Instantiating generic methods
+            GenericMethodExample<string> genericMethodExample1 = new GenericMethodExample<string>();
+            genericMethodExample1.Method1(10, "Lakshmi");
+            GenericMethodExample<int> genericMethodExample2 = new GenericMethodExample<int>();
+            genericMethodExample2.Method1(10, 20);
+        }
+    }
+    //Generic class example
+    //First define class using generic syntax
+    public  class CustomerData<TName,TId>
+        {
+
+        //generic fields
+         public TName name { get; set; }
+         public TId id { get; set; }
+
+        }
+
+}
